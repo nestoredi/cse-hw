564963db1229eaf31e212bac6b421cfde32386ab
nestoredi <86657718+nestoredi@users.noreply.github.com>
2024-06-10 23:17:38 -0300

    finish prep2 basic

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage : ");
+
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage>= 80)
+        {
+            letter = "B";  
+        }
+        else if (percentage>= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($" your grade is {letter}");
+        if (percentage >= 70 )
+        {
+            Console.WriteLine(" You passed. Congrats!!!");
+        }
+        else
+        {
+            Console.WriteLine(" Next time better luck");
+        }
     }
 }
