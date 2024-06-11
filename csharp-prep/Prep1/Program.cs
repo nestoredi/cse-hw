4203724c5a341f502e6c38d9c69668170bec5703
nestoredi <86657718+nestoredi@users.noreply.github.com>
2024-06-10 22:41:48 -0300

    ask for your name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // ask for your name
         Console.Write("What is your first name?: ");
         string first = Console.ReadLine();
         Console.Write("What is your last name? : ");
