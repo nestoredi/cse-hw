1608568d04a66efb79741c45713d880e0daf78f1
nestoredi <86657718+nestoredi@users.noreply.github.com>
2024-06-15 17:30:26 -0300

    week 02 asigm

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._jobTitle = "Senior developer";
+       job1._company = "Cisco";
+       job1._starYear = 2018;
+       job1._endYear = 2023;
+
+       Job job2 = new Job();
+       job2._jobTitle = "Junior developer";
+       job2._company = "Oracle";
+       job2._starYear = 2015;
+       job2._endYear = 2018;
+
+       Resume myResume = new Resume();
+       myResume._name = "Scott Red";
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+
+       myResume.Display();
+
     }
 }
